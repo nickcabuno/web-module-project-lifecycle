3e7bba56527e7cd0520bd2dba1111c935f5a9ef0
nickcabuno <nickcabuno17@gmail.com>
2021-04-07 05:10:06 -0400

    styled

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React from 'react';
+import styled from 'styled-components';
 import getHub from './getHub'
 
+const StyledHub = styled.div`
+box-shadow: 0px 10px 50px 20px rgba(0, 0, 0, 0.2);
+max-width: 300px;
+margin: auto;
+display: flex;
+align-items: center;
+flex-flow: column nowrap;
+margin-top: 3%;
+color: grey;
+background-color: tan;
+
+.avi{
+    width: 60%;
+    margin-top: 3%;
+}
+`
 
 class App extends React.Component {
     state = {
-        info: []
+        info: [],
     }
     
-    GithubInfo() {
+    componentDidMount() {
       getHub()
           .then(data => {
               this.setState({
@@ -18,19 +35,19 @@ class App extends React.Component {
               console.log(err);
           });
         }
+    
 
   render() {
       return(
-      <div>
-          <h1>GitHub Info</h1>
-          <div>{this.state.info.avatar_url}</div>
-          <h2>{this.state.info.name}</h2>
-          <p>{this.state.info.login}</p>
-          <p>{this.state.info.followers}</p>
-          <p>{this.state.info.following}</p>
-          <p>{this.state.info.location}</p>
-          <p>{this.state.info.bio}</p>
-      </div>
+      <StyledHub>
+        <img className="avi" src={this.state.info.avatar_url}></img>  
+        <h2>{this.state.info.name}</h2>
+        <p>{this.state.info.login}</p>
+        <p>Followers: {this.state.info.followers}</p>
+        <p>Following: {this.state.info.following}</p>
+        <p>Location: {this.state.info.location}</p>
+        <p>{this.state.info.bio}</p>
+      </StyledHub>
       );
   }
 }
